5e2c46a122f78a616449265d34aac552598c65ea
raphaelakinwale56-byte <raphaelakinwale56@gmail.com>
2026-03-13 06:24:42 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,29 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: './', // 
+  const env = loadEnv(mode, process.cwd(), "");
 
+  return {
     plugins: [react(), tailwindcss()],
 
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
     },
 
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        "@": path.resolve(__dirname, "."),
       },
     },
 
     server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
+      proxy: {
+        "/api": "http://localhost:3000",
+      },
+      hmr: env.DISABLE_HMR !== "true",
     },
   };
 });
